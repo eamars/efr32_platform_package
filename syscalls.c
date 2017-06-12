e61cf87a1ec30bc4734209a28ed619ab3f226fe4
Ran Bao <ran.bao@wirelessguard.co.nz>
2017-06-13 09:58:11 +1200

    Add basic system calls

---
+++ syscalls.c
@@ -0,0 +1,14 @@
+/**
+ * @file syscalls.c
+ * @brief Basic implementations for syscalls
+ * @author Ran Bao
+ * @date June, 2017
+ */
+
+void _exit(int extno __attribute__ ((unused)))
+{
+	while (1)
+	{
+
+	}
+}
