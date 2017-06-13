cadeef2bb2c6ee2ed0c2f30951b0d7a2d8fb6cb4
Ran Bao <ran.bao@wirelessguard.co.nz>
2017-06-14 09:59:38 +1200

    Include Zigbee 5.10

---
+++ protocol/zigbee_5.10/documentation/120-3023-000_AF_V2_API/structEmberAfPriceClientBlockThreshold.js
@@ -0,0 +1,7 @@
+var structEmberAfPriceClientBlockThreshold =
+[
+    [ "issuerTariffId", "structEmberAfPriceClientBlockThreshold.html#a225e733eea37dcb4ff8439bb6d02f1a5", null ],
+    [ "subPayloadControl", "structEmberAfPriceClientBlockThreshold.html#ab5c0d29faea26f213ec7d1290e808c09", null ],
+    [ "tierNumberOfBlockThresholds", "structEmberAfPriceClientBlockThreshold.html#ac99ae5142dcbc2de33761b97ea17aa84", null ],
+    [ "blockThreshold", "structEmberAfPriceClientBlockThreshold.html#ae0abcb848a5f79f6624af2f4983f49ca", null ]
+];
