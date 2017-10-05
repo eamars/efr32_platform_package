30db923d87cdf75cc9d92899f92c2bb08b34e634
Ran Bao <ran.bao@wirelessguard.co.nz>
2017-10-05 16:58:16 +1300

    Add C++ guard

--- drivers/delay/delay.h
+++ drivers/delay/delay.h
@@ -7,9 +7,18 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void delay_init(void);
 
 void delay_ms(uint32_t ms);
+
 void delay_us(uint32_t us);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // DELAY_H_
