7c9094ff574c33e4df18ce0bd9e1dbc53f0693b6
Ran Bao <ran.bao@wirelessguard.co.nz>
2017-10-11 10:43:37 +1300

    Provide alternative bytes interface for raw subg packet

--- common/subg_packet.h
+++ common/subg_packet.h
@@ -9,17 +9,28 @@
 
 #include <stdint.h>
 
-typedef struct __attribute__ ((packed))
+
+#define SUBG_PACKET_TOTAL_LEN (12) // bytes
+
+typedef union
 {
-	uint8_t version;
-	uint8_t device_id8;
-	int16_t lpk_rssi;
-	int8_t lpk_snr;
-	int8_t battery_percentage;
-	int8_t temperature;
-	uint8_t door_state;
-	uint16_t compass_value;
-	uint16_t heading;
+	// byte interface
+	uint8_t _raw_data[SUBG_PACKET_TOTAL_LEN];
+
+	// struct interface
+	struct __attribute__ ((packed))
+	{
+		uint8_t version;
+		uint8_t device_id8;
+		int16_t lpk_rssi;
+		int8_t lpk_snr;
+		int8_t battery_percentage;
+		int8_t temperature;
+		uint8_t door_state;
+		uint16_t compass_value;
+		uint16_t heading;
+	};
+
 } subg_packet_t;
 
 
