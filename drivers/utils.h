ffaf49ea43884a37361ede3a6d82b7877b3fd6f4
Ran Bao <ran.bao@wirelessguard.co.nz>
2017-10-25 16:21:01 +1300

    Introduce utils header

---
+++ drivers/utils.h
@@ -0,0 +1,16 @@
+/**
+ * @brief Utility functions
+ * @file utils.h
+ * @author Ran Bao
+ * @date 25, Oct, 2017
+ */
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+
+#define MIN( a, b ) ( ( ( a ) < ( b ) ) ? ( a ) : ( b ) )
+#define MAX( a, b ) ( ( ( a ) > ( b ) ) ? ( a ) : ( b ) )
+
+
+#endif // UTILS_H_
