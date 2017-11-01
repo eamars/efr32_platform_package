d2d700ad76e7a00b1c16ac4b229b08868f14cc64
Ran Bao <ran.bao@wirelessguard.co.nz>
2017-11-01 16:58:45 +1300

    Add a new packet type that reports  reset information

--- common/subg_packet_v2.h
+++ common/subg_packet_v2.h
@@ -13,11 +13,14 @@
 #define SUBG_PACKET_V2_HEADER_SIZE (sizeof(subg_packet_v2_header_t))
 #define SUBG_PACKET_V2_DATA_SIZE ((12) + SUBG_PACKET_V2_HEADER_SIZE)
 #define SUBG_PACKET_V2_CMD_SIZE ((6) + SUBG_PACKET_V2_HEADER_SIZE)
+#define SUBG_PACKET_V2_DEBUG_SIZE (255)
+#define SUBG_PACKET_V2_DEBUG_MAX_PAYLOAD_LEN (SUBG_PACKET_V2_DEBUG_SIZE - SUBG_PACKET_V2_HEADER_SIZE - 1)
 
 typedef enum
 {
 	SUBG_PACKET_V2_TYPE_DATA = 0x00,
-	SUBG_PACKET_V2_TYPE_CMD = 0x01
+	SUBG_PACKET_V2_TYPE_CMD = 0x01,
+	SUBG_PACKET_V2_TYPE_DEBUG = 0x02
 } sugb_packet_v2_type_list_t;
 
 typedef enum
@@ -70,5 +73,19 @@ typedef union
 		uint32_t payload;
 	};
 } subg_packet_v2_cmd_t;
+
+typedef union
+{
+	uint8_t _raw_data[SUBG_PACKET_V2_DEBUG_SIZE];
+
+	struct __attribute__ ((packed))
+	{
+		subg_packet_v2_header_t header;
+
+		// payload
+		uint8_t payload_length;
+		uint8_t payload[SUBG_PACKET_V2_DEBUG_MAX_PAYLOAD_LEN];
+	};
+} subg_packet_v2_debug_t;
 
 #endif // SUBG_PACKET_V2_H_
