58dba23178d9fa828bb5d7c75ad501b4ca1fb2d3
Ran Bao <ran.bao@wirelessguard.co.nz>
2017-11-24 12:10:05 +1300

    provide debug print function

--- drivers/drv_debug.h
+++ drivers/drv_debug.h
@@ -12,4 +12,15 @@
 #define DRV_ASSERT(x) {} while (1)
 #endif
 
+// print
+#include <stdio.h>
+#include "irq.h"
+#include "yield.h"
+
+#define DEBUG_PRINT(fmt, args...) YIELD(if (__IS_INTERRUPT()) DRV_ASSERT(false); else \
+        fprintf(stderr, "DEBUG: %s:%d: " fmt, __func__, __LINE__, ##args);)
+
+#define ERROR_PRINT(fmt, args...) YIELD(if (__IS_INTERRUPT()) DRV_ASSERT(false); else \
+        fprintf(stderr, "ERROR: %s:%d: " fmt, __func__, __LINE__, ##args);)
+
 #endif /* DRV_DEBUG_H_ */
