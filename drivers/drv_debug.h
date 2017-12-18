a164cd0878aa45f65925c132bd835619ac598218
Ran Bao <ran.bao@wirelessguard.co.nz>
2017-12-18 13:25:52 +1300

    Implement backtrace without debugger.

--- drivers/drv_debug.h
+++ drivers/drv_debug.h
@@ -6,8 +6,21 @@
 #define DRV_DEBUG_H_
 
 #include BOARD_HEADER
+#include <stdint.h>
+
+typedef struct
+{
+    uint32_t max_depth;
+    uint32_t current_depth;
+    uint32_t * ip_stack;
+    uint32_t * code_stack;
+} unwind_ctrl_t;
 
 // assert
+void assert_efr32(const char * file, uint32_t line);
+
+void backtrace(unwind_ctrl_t * ctrl);
+
 #ifndef DRV_ASSERT
 #define DRV_ASSERT(x) {} while (1)
 #endif
