9e38ae114c1aac06ccf4cb6dd6d5d79b93b51a54
Ran Bao <ran.bao@wirelessguard.co.nz>
2018-01-29 16:14:12 +1300

    rename file

---
+++ drivers/mfg_hw_test/mfg_hw_test_main.c
@@ -0,0 +1,44 @@
+/**
+ * @brief Manufacture Hardware Test implementation
+ * @file mfg_hw_test_menu.c
+ * @date 29/Jan/2018
+ * @author Ran Bao
+ */
+#include "em_device.h"
+#include "reset_info.h"
+
+
+void mfg_generic_irq_handler(uint32_t ipsr)
+{
+    IRQn_Type irqno = (IRQn_Type) ((ipsr & 0x1ff) - 16);
+
+    while (1)
+    {
+
+    }
+}
+
+void hw_test_0(void)
+{
+
+}
+
+void hw_test_1(void)
+{
+
+}
+
+void mfg_main(void)
+{
+    // clear the reset info
+    reset_info_clear();
+
+    // trigger a divide by 0 error
+    uint32_t a = 5/0;
+
+    while (1)
+    {
+
+    }
+}
+
