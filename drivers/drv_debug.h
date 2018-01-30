cd7bb48a0d8a06a9adeed0974735740eb6176542
Ran Bao <ran.bao@wirelessguard.co.nz>
2018-01-30 15:02:06 +1300

    Allow DEBUG_PRINT and ERROR_PRINT to be overwritten in device specific header

--- drivers/drv_debug.h
+++ drivers/drv_debug.h
@@ -41,18 +41,23 @@ void debug_breakpoint(void);
 
 
 #ifndef DRV_ASSERT
-#define DRV_ASSERT(x) while (1) {}
-#endif
+extern void assert_failed(const char * file, uint32_t line);
+#define DRV_ASSERT(x) ((x) ? ((void) 0) : assert_failed(__FILE__, __LINE__))
+#endif // DRV_ASSERT
 
 // print
 #include <stdio.h>
 #include "irq.h"
 #include "yield.h"
 
+#ifndef DEBUG_PRINT
 #define DEBUG_PRINT(fmt, args...) YIELD(if (__IS_INTERRUPT()) DRV_ASSERT(false); else \
         fprintf(stderr, "DEBUG: %s:%d: " fmt, __func__, __LINE__, ##args);)
+#endif // DEBUG_PRINT
 
+#ifndef ERROR_PRINT
 #define ERROR_PRINT(fmt, args...) YIELD(if (__IS_INTERRUPT()) DRV_ASSERT(false); else \
         fprintf(stderr, "ERROR: %s:%d: " fmt, __func__, __LINE__, ##args);)
+#endif // ERROR_PRINT
 
 #endif /* DRV_DEBUG_H_ */
