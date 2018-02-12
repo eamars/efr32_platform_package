73e36b710e88351004ee75b96908519de840090a
Ran Bao <ran.bao@wirelessguard.co.nz>
2018-02-12 16:23:30 +1300

    Add more examples showing how to integrate your code with mfg mode

--- drivers/mfg_hw_test/mfg_hw_test_main.c
+++ drivers/mfg_hw_test/mfg_hw_test_main.c
@@ -6,6 +6,7 @@
  */
 #include "em_device.h"
 #include "reset_info.h"
+#include "mfg_hw_test.h"
 
 
 void mfg_generic_irq_handler(uint32_t ipsr)
@@ -18,23 +19,36 @@ void mfg_generic_irq_handler(uint32_t ipsr)
     }
 }
 
-void hw_test_0(void)
-{
-
-}
-
-void hw_test_1(void)
-{
-
-}
 
 void mfg_main(uint32_t * boot_flags)
 {
     // clear the reset info
     reset_info_clear();
 
-    // trigger a divide by 0 error
-//    uint32_t a = 5/0;
+    uint32_t mfg_entry = boot_flags[0];
+    switch (mfg_entry)
+    {
+        case 0:
+        {
+            // this is the first default entry for mfg mode.
+            // to jump to next mfg mode, we can call with api
+            mfg_hw_test_reboot_to_enter(mfg_entry + 1);
+
+            // make sure the code will never go though this point
+            break;
+        }
+        case 127:
+        {
+            // say 127 is the imu temperature coefficient calibration
+            // do something
+            // write to eeprom
+            // reset
+            break;
+        }
+
+        default:
+            break;
+    }
 
     while (1)
     {
