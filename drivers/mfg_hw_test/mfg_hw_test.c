c639de6360fa51ad9413b47b7e2af99ecf4bfdac
Ran Bao <ran.bao@wirelessguard.co.nz>
2018-02-12 15:54:26 +1300

    add boot option flags to allow mfg mode to be selected in the application

--- drivers/mfg_hw_test/mfg_hw_test.c
+++ drivers/mfg_hw_test/mfg_hw_test.c
@@ -4,13 +4,18 @@
  * @date 29/Jan/2018
  * @author Ran Bao
  */
-
+#include "reset_info.h"
 #include "mfg_hw_test.h"
 #include "bootloader_api.h"
 
 extern irq_handler_t mfg_test_vector_table[];
 
-void mfg_hw_test_reboot_to_enter(void)
+void mfg_hw_test_reboot_to_enter(uint32_t entry)
 {
+    // use first boot flag to indicate the entry
+    boot_info_map_t * boot_info_map = (boot_info_map_t *) reset_info_read();
+    boot_info_map->boot_flags[0] = entry;
+
+    // reboot to mfg vector table
     reboot_to_addr((uint32_t) mfg_test_vector_table);
 }
